6fa7e7b14715e511e8d46de47f381da4f419c220
Andrey <sazonov31294@gmail.com>
2023-06-02 17:39:41 +0300

    Функция нахождения случайного число с плавающей точкой

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Функция, возвращающая случайное целое число из переданного диапазона включительно
 function getRandomInteger(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -6,4 +7,12 @@ function getRandomInteger(min, max) {
   }
 }
 
+//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
+function getRandomWithDot(min, max, digits) {
+  if (min >= 0 && max >= 0 && max > min) {
+    return ((Math.random() * (max - min)) + min).toFixed(digits);
+  }
+}
+
 getRandomInteger(0, 10);
+console.log(getRandomWithDot(0, 10, 3));
