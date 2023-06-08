70f0bd443896aa11f26858ab9b817043ac185ede
Andrey <sazonov31294@gmail.com>
2023-06-08 15:44:22 +0300

    Правка функций для случайного целого числа и числа с плавающей точкой

--- js/main.js
+++ js/main.js
@@ -2,16 +2,18 @@
 function getRandomInteger(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  if (min >= 0 && max >= 0 && max > min) {
+  if (min >= 0 && max >= 0 && max > min && min !== String && max !== String) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+  return 'Исходные данные неверны';
 }
 
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
 function getRandomWithDot(min, max, digits) {
-  if (min >= 0 && max >= 0 && max > min) {
-    return ((Math.random() * (max - min)) + min).toFixed(digits);
+  if (min >= 0 && max >= 0 && max > min && min !== String && max !== String) {
+    return Number(((Math.random() * (max - min)) + min).toFixed(digits));
   }
+  return 'Исходные данные неверны';
 }
 
 getRandomInteger(0, 10);
